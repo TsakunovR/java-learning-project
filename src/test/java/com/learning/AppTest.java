bc7d5008d4e6b9713f0eff64edb827cd66a2c316
w0zhk <antnnnnnn98@gmail.com>
2026-02-21 19:19:52 +0300

    assertEquals simplufied to assertTrue

--- src/test/java/com/learning/AppTest.java
+++ src/test/java/com/learning/AppTest.java
@@ -5,6 +5,7 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Простой тест для метода add класса App.
@@ -44,6 +45,6 @@ class AppTest {
         assertEquals (198772, response.jsonPath().getInt("petId"));
         assertEquals (7, response.jsonPath().getInt("quantity"));
         assertEquals ("approved", response.jsonPath().getString("status"));
-        assertEquals (true, response.jsonPath().getBoolean("complete"));
+        assertTrue(response.jsonPath().getBoolean("complete"));
     }
 }
