7adaab41005eb0274c2274d108d8c4f34eb97998
w0zhk <antnnnnnn98@gmail.com>
2026-02-21 19:18:16 +0300

    restAssured added Order autotest Java added by w0zhk

--- src/test/java/com/learning/AppTest.java
+++ src/test/java/com/learning/AppTest.java
@@ -1,5 +1,7 @@
 package com.learning;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,5 +16,34 @@ class AppTest {
         assertEquals(5, App.add(2, 3));
         assertEquals(0, App.add(-1, 1));
         assertEquals(-5, App.add(-2, -3));
+        assertEquals(-145, App.add(2, -147));
+    }
+    @Test
+    void createStoreOrderTest() {
+        RestAssured.baseURI = "https://petstore.swagger.rv-school.ru/api/v3";
+        String requestBody = """
+                {
+                "id":10,
+                "petId":198772,
+                "quantity":7,
+                "shipDate":"2026-02-21T15:23:48.515Z",
+                "status":"approved",
+                "complete":true
+                }
+                """;
+        Response response = RestAssured
+                .given()
+                .header("Content-Type","application/json")
+                .body(requestBody)
+                .when()
+                .post("/store/order")
+                .then()
+                .extract().response();
+        assertEquals (200, response.statusCode(), "Неверный статус код");
+        assertEquals (10, response.jsonPath().getInt("id"), "Неверный id заказа");
+        assertEquals (198772, response.jsonPath().getInt("petId"));
+        assertEquals (7, response.jsonPath().getInt("quantity"));
+        assertEquals ("approved", response.jsonPath().getString("status"));
+        assertEquals (true, response.jsonPath().getBoolean("complete"));
     }
 }
