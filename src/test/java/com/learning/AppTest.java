c8d0e4fe82e2385b0301c6512033382450ee46b7
NadezhdaKhasanshina <nadezhda.khasanshina@mail.ru>
2026-02-21 23:06:47 +0300

    Добавлен тест для создания питомца

--- src/test/java/com/learning/AppTest.java
+++ src/test/java/com/learning/AppTest.java
@@ -1,5 +1,7 @@
 package com.learning;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,5 +16,44 @@ class AppTest {
         assertEquals(5, App.add(2, 3));
         assertEquals(0, App.add(-1, 1));
         assertEquals(-5, App.add(-2, -3));
+    }
+
+    @Test
+    void addReturnsSumOfTwoNumbersWithZero() {
+        assertEquals(-10, App.add(-10, 0));
+        assertEquals(99, App.add(-1, 100));
+        assertEquals(8, App.add(-22, 30));
+    }
+
+    @Test
+    void createNewPetTest() {
+        RestAssured.baseURI = "https://petstore.swagger.rv-school.ru/api/v3";
+
+        //Подготавливаем данные для создания питомца (из задания)
+        String requestBody = """
+        {
+          "id": 1,
+          "name": "Buddy",
+          "status": "available"  
+        }  
+    """;
+
+        //Отправляем POST-запрос и получаем ответ
+        Response response = RestAssured
+                .given()
+                .header("Content-Type", "application/json")
+                .body(requestBody)
+                .when()
+                .post("/pet")
+                .then()
+                .extract().response();
+
+        //Проверяем статус код
+        assertEquals(200, response.statusCode(), "Неверный статус код");
+
+        //Проверяем, что ответ содержит данные созданного питомца
+        assertEquals(1, response.jsonPath().getInt("id"), "Неверный id питомца");
+        assertEquals("Buddy", response.jsonPath().getString("name"), "Неверное имя питомца");
+        assertEquals("available", response.jsonPath().getString("status"), "Неверный статус питомца");
     }
 }
