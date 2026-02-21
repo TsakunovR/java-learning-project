80997d177097eced881f2c6af16f5f4570ddbc27
Dominik <shevchikdominik@email.com>
2026-02-21 22:39:55 +0300

    повторил за Романом

--- src/test/java/com/learning/AppTest.java
+++ src/test/java/com/learning/AppTest.java
@@ -1,8 +1,12 @@
 package com.learning;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
+import static jdk.internal.org.jline.utils.Colors.s;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Простой тест для метода add класса App.
@@ -14,5 +18,42 @@ class AppTest {
         assertEquals(5, App.add(2, 3));
         assertEquals(0, App.add(-1, 1));
         assertEquals(-5, App.add(-2, -3));
+    }
+    @Test
+    void addReturnsSumOfTwoNumbers2() {
+        assertEquals(27, App.add(8, 19));
+        assertEquals(-99, App.add(-100, 1));
+        assertEquals(-5, App.add(-2, -3));
+    }
+    @Test
+    void createStorOrderTest() {
+        RestAssured.baseURI = "https://petstore.swagger.rv-school.ru/api/v3";
+
+        String requestBody = """
+                {
+                  "id": 10,
+                  "petId": 198772,
+                  "quantity": 7,
+                  "shipDate": "2026-02-21T19:04:35.276Z",
+                  "status": "approved",
+                  "complete": true
+                  }
+                """;
+        Response response = RestAssured
+                .given()
+                .header("Content-Type","application/json")
+                .body(requestBody)
+                .when()
+                .post("/store/order")
+                .then()
+                .extract().response();
+
+        assertEquals(200, response.statusCode(), "неверный статус код");
+        assertEquals(10, response.jsonPath().getInt("id"), "неверный id заказа");
+        assertEquals(198772, response.jsonPath().getInt("petId"), "неверный petId заказа");
+        assertEquals(7, response.jsonPath().getInt("quantity"));
+        assertEquals("approved", response.jsonPath().getString("status"));
+        assertTrue(response.jsonPath().getBoolean("complete"));
+
     }
 }
