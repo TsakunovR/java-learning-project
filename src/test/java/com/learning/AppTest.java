ed2d7c6dae551324b598a56e4628646f758581f9
w0zhk <antnnnnnn98@gmail.com>
2026-02-23 19:02:21 +0300

    Поменял название класса  добавления питомца на createPetTest

--- src/test/java/com/learning/AppTest.java
+++ src/test/java/com/learning/AppTest.java
@@ -5,7 +5,6 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Простой тест для метода add класса App.
